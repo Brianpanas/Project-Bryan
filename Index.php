30b7fce72a4759b486f7a936ba12eadd1470293f
Brianpanas <brianpanasgaming@gmail.com>
2024-11-24 23:41:31 +0800

    Minor adjustment
    deleted some extra dots in index.

--- Index.php
+++ Index.php
@@ -12,11 +12,11 @@
             id="darkModeToggleRB"
             onclick="controller_Btn_DarkMode()"
             title="Toggle light/dark mode"
-            data-light-img="../../src/light-mode.png"
-            data-dark-img="../../src/night-mode.png"
-            style="background-image: url('../../src/light-mode.png'); background-size: cover; background-position: center; background-repeat: no-repeat;">
+            data-light-img="./src/light-mode.png"
+            data-dark-img="./src/night-mode.png"
+            style="background-image: url('./src/light-mode.png'); background-size: cover; background-position: center; background-repeat: no-repeat;">
         </button>
-        
+
     </div>
     <script type="module" src="./ProjectBry/Scripts/Controller_Btn_DarkMode.js"></script>
 </body>
