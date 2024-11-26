e758338b8c3a9e2de4f14b08cf357c45a3da24b4
Brianpanas <brianpanasgaming@gmail.com>
2024-11-26 17:40:19 +0800

    Added Button RB
    New button project

---
+++ ProjectBry/Scripts/Controller_Btn_ButtonRB.js
@@ -0,0 +1,16 @@
+/* Import */
+
+/* Import */
+
+/* Controller */
+function controller_Btn_ButtonRB() {
+    alert('Button clicked!');
+}
+/* Controller */
+
+/* Declare Global */
+window.controller_Btn_ButtonRB = controller_Btn_ButtonRB;
+/* Declare Global */
+
+/* Export */
+export default controller_Btn_ButtonRB;
