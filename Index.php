6e3f22447097541f6e8a7f668c5aa66e53ad3c5f
Brianpanas <brianpanasgaming@gmail.com>
2024-11-27 16:06:33 +0800

    Added Category Button
    New things added. Check it out!

--- Index.php
+++ Index.php
@@ -3,7 +3,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dark Mode Toggle</title>
-    <link rel="stylesheet" href="ProjectBry/CSS/PBStyle.css">
+    <link rel="stylesheet" href="./Sample Page/CSS/index.css">
 </head>
 <body>
 
@@ -18,7 +18,23 @@
         </button>
     </div>
 
+    <div class="button-container-RB">
+        <button 
+            id="customButtonRB" 
+            class="custom-button-RB" 
+            style="background: url('./src/turquoise bg button.png') no-repeat center center; background-size: cover;"
+            onclick="controller_Btn_ButtonRB()">
+            <span class="button-text-RB">Click Me</span>
+            <img src="./src/light-mode.png" alt="Button Icon" class="button-icon-RB">
+        </button>
+    </div>
 
+    <div class="cat-buttonRB-container">
+        <button class="category-buttonRB">Category Button 1</button>
+        <button class="category-buttonRB">Category Button 2</button>
+        <button class="category-buttonRB">Category Button 3</button>
+        <button class="category-buttonRB">Category Button 4</button>
+    </div>
 
     </div>
     <script type="module" src="./ProjectBry/Scripts/Controller_Btn_DarkMode.js"></script>
