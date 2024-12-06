17001acb3c00490e7196aca0356511f481ef2532
Brianpanas <brianpanasgaming@gmail.com>
2024-12-06 15:18:24 +0800

    Additional Buttons

--- Index.php
+++ Index.php
@@ -17,22 +17,63 @@
         </button>
 
 
-    <button 
-        class="custom-button-RB" 
-        onclick="controller_Btn_ButtonRB()">
-        <span class="button-text-RB">Click Me</span>
-        <img src="./src/light-mode.png" alt="Button Icon" class="button-icon-RB">
-    </button>
+    <div class="custom-button-RB" onclick="controller_Btn_ButtonRB()">
+        <div class="button-text-RB">Click Me</div>
+        <div class="button-icon-RB" style="background-image: url('./src/light-mode.png'); background-size: 30px;; background-position: center; background-repeat: no-repeat;">
+        </div>
+    </div>
 
-    <div class="cat-buttonRB-container">
-        <button class="category-buttonRB">Category Button 1</button>
-        <button class="category-buttonRB">Category Button 2</button>
-        <button class="category-buttonRB">Category Button 3</button>
-        <button class="category-buttonRB">Category Button 4</button>
+    <div class="DashboardCardRB">
+        <div class="cardRB">
+            <div class="card-headerRB">
+                <div class="card-titleRB">
+                    <div class="card-logoRB" style="background-image: url('./src/light-mode.png');"></div>
+                    <div  class="CardheaderRB">Card Dashboard 1</div>
+                </div>
+                <p class="card-header-descriptionRB">Frontend Development</p>
+                <p class="card-header-descriptionRB2">Run baby run don't ever look back. They'll tear us apart if you give them a chance.</p>
+            </div>
+            <div class="card-progressRB">
+                <div class="progress-barRB" style="width: 70%;"></div>
+            </div>
+            <div class="card-footerRB">
+                <div class="card-dateRB">Dec 05</div>
+                <div class="teamRB">
+                    <img src="./src/ariel.jpg">
+                    <img src="./src/bector.jpg">
+                    <img src="./src/bry.jpg">
+                </div>
+            </div>
+        </div>
+
+        <div class="cardRB">
+            <div class="card-headerRB">
+                <div class="card-titleRB">
+                    <div class="card-logoRB" style="background-image: url('./src/night-mode.png');"></div>
+                    <div class="CardheaderRB">Card Dashboard 2</div>
+                </div>
+                <p class="card-header-descriptionRB">Design</p>
+                <p class="card-header-descriptionRB2">Run baby run don't ever look back. They'll tear us apart if you give them a chance.</p>
+            </div>
+            <div class="card-progressRB">
+                <div class="progress-barRB" style="width: 40%;"></div>
+            </div>
+            <div class="card-footerRB">
+                <div class="card-dateRB">Nov 29</div>
+                <div class="teamRB">
+                    <img src="./src/rogie.jpg">
+                    <img src="./src/oh2.jpg">
+                    <img src="./src/os.jpg">
+                </div>
+            </div>
+        </div>
+
+        <!-- Add more here -->
     </div>
 
     </div>
     <script type="module" src="./ProjectBry/Scripts/Controller_Btn_DarkMode.js"></script>
+    <script type="module"  src="./ProjectBry/Scripts/Controller_Btn_ButtonRB.js"></script>
 </body>
 </html>
 
