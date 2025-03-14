c617fc74268dec1e6cc10113adece379cc293955
Brianpanas <brianpanasgaming@gmail.com>
2025-03-15 00:29:35 +0800

    Added new components
    Vertical Progress Tracker component added

--- Index.php
+++ Index.php
@@ -111,7 +111,41 @@
     </div>
     <!-- RBTP-tabs -->
 
+    <!-- Vertical Progress Tracker -->
+    <div class="rbpt-container">
+        <div class="rbpt-line"></div>
 
+        <div class="rbpt-step">
+            <img src="./src/checkIcon.png" alt="Tracker">
+            <div class="rbpt-text">Order Placed</div>
+        </div>
+
+        <div class="rbpt-step">
+            <img src="./src/checkIcon.png" alt="Tracker">
+            <div class="rbpt-text">Shipped</div>
+        </div>
+
+        <div class="rbpt-step">
+            <img src="./src/checkIcon.png" alt="Tracker">
+            <div class="rbpt-text">Out for Delivery</div>
+        </div>
+
+        <div class="rbpt-step">
+            <img src="./src/checkIcon.png" alt="Tracker">
+            <div class="rbpt-text">add here</div>
+        </div>
+
+        <div class="rbpt-step">
+            <img src="./src/checkIcon.png" alt="Tracker">
+            <div class="rbpt-text">add here</div>
+        </div>
+
+        <div class="rbpt-step">
+            <img src="./src/complete icon purple.png" alt="Progress">
+            <div class="rbpt-text rbpt-last-step">Delivered</div>
+        </div>
+    </div>
+    <!-- Vertical Progress Tracker -->
 
     </div>
     <script type="module" src="./ProjectBry/Scripts/Controller_Btn_DarkMode.js"></script>
